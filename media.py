5383d891415dc57412b6aaf983d3dcc02d5bd8b3
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-04-01 00:33:00 +0500

    Update media.py

--- media.py
+++ media.py
@@ -1,36 +1,11 @@
-import fresh_tomatoes
-import media
+import webbrowser
 
-toy_story = media.Movie("Toy Story",
-	"The story of a boy and his toys that come to lofe",
-	"https://upload.wikimedia.org/wikipedia/ru/a/a6/Toy_Story_1995_Poster.jpg",
-	"http://www.youtube.com/watch?v=KYz2wyBy3kc")
+class Movie():
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
 
-#print(toy_story.storyline)
-avatar = media.Movie("Avatar",
-	"A marine on an alien planet",
-	"https://upload.wikimedia.org/wikipedia/ru/thumb/4/4b/Avatar-2009.jpg/640px-Avatar-2009.jpg",
-	"http://www.youtube.com/watch?v=cRdxXPV9GNQ")
-
-equilibrium = media.Movie("Equilibrium",
-	"In a Fascist future where all forms of feeling are illegal",
-	"https://upload.wikimedia.org/wikipedia/ru/4/4f/Equilibrium.jpg",
-	"http://www.youtube.com/watch?v=ZVDiaYQXBVs")
-
-big_lebowski = media.Movie("The Big Lebowski",
-                           "Times Like These Call For A Big Lebowski",
-                           "http://upload.wikimedia.org/wikipedia/en/3/35/Biglebowskiposter.jpg",
-                           "https://www.youtube.com/watch?v=cd-go0oBF4Y")
-
-got = media.Movie("Game of Thrones",
-                     "7 powerful Houses are fighting for the Iron Throne of 7 Kingdoms",
-                     "http://4.bp.blogspot.com/-FuP3ehGClQs/TfBm4yiOSVI/AAAAAAAAA38/6Wgx0eijnKI/s1600/600full-game-of-thrones-poster.jpg",
-                     "http://www.youtube.com/watch?v=BpJYNVhGf1s")
-
-monster = media.Movie("Monsters University", 
-                 "A look at the relationship between Mike and Sulley at Monsters University",
-                 "http://upload.wikimedia.org/wikipedia/en/2/2a/Monsters_University_poster_3.jpg",
-                 "http://www.youtube.com/watch?v=xBzPioph8CI")
-
-movies = [toy_story, avatar, equilibrium, big_lebowski, got, monster]
-fresh_tomatoes.open_movies_page(movies)
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
